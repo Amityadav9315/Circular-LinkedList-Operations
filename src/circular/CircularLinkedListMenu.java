b61193b744cf4add332248d81cc812c87276c1b6
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-11 11:54:04 +0530

    circular Linked List

---
+++ src/circular/CircularLinkedListMenu.java
@@ -0,0 +1,23 @@
+package circular;
+
+import java.util.Scanner;
+
+public class CircularLinkedListMenu {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        CircularLinkedList list=new CircularLinkedList();
+        int choice,data;
+        do{
+            System.out.println("\n Doubly linked List Operations:");
+            System.out.println("1. Add at Front");
+            System.out.println("2. Add at End");
+            System.out.println("3. Delete from Front");
+            System.out.println("4. Delete fron End");
+            System.out.println("5. Display from Front");
+            System.out.println("6. Display from End");
+            System.out.println("7. Search data");
+        }
+
+    }
+}
